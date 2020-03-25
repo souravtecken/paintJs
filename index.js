f63558c90cfaac8e6db533761ffc429c51f91d1b
souravtecken <souravtecken@gmail.com>
2020-03-25 13:30:11 +0530

    Slightly improved performance of paintBucket tool

--- index.js
+++ index.js
@@ -19,30 +19,50 @@ function drawRectangle(ctx, x, y, width, height){
     ctx.strokeRect(x, y, width, height);
 }
 
-function paintBucket(ctx, x, y){    
-    const originPixelColor = ctx.getImageData(x, y, 1, 1).data;
-    const pixelQueue = new Array([x, y]); // Initialise queue with initial coords
+function paintBucket(ctx, x, y){
+    // TODO: Don't paint if origin color's same as brush color
+    // TODO: Take in color from canvas fill color
     const canvasHeight = ctx.canvas.clientHeight;
     const canvasWidth = ctx.canvas.clientWidth;
-    const imageData = ctx.getImageData(0, 0, canvasHeight, canvasWidth);
-    console.log(imageData);
-    if(x >= 0 && x < canvasWidth && y >= 0 && y < canvasHeight){        
-        while(pixelQueue.length != 0){        
-            const coord = pixelQueue.pop();
-            x = coord[0];
-            y = coord[1];
-            if(x-1 >= 0 && compareColors(ctx.getImageData(x-1, y, 1, 1).data, originPixelColor))
-                pixelQueue.push([x-1, y]);
-            if(x+1 < canvasWidth && compareColors(ctx.getImageData(x + 1, y, 1, 1).data, originPixelColor))
-                pixelQueue.push([x+1, y]);
-            if(y-1 >= 0 && compareColors(ctx.getImageData(x, y-1, 1, 1).data, originPixelColor))
-                pixelQueue.push([x, y-1]);
-            if(y+1 < canvasHeight && compareColors(ctx.getImageData(x, y+1, 1, 1).data, originPixelColor))
-                pixelQueue.push([x, y+1]);
-            ctx.fillRect(x, y, 1, 1);
-        }
-    }   
-    console.log(imageData); 
+    const pixelColorGrid = ctx.getImageData(0, 0, canvasHeight, canvasWidth);
+    const color = [0, 0, 0, 255];
+    const originPixelColor = [0, 0, 0, 0];
+
+    const compareColors = (index) => {        
+        return (
+            pixelColorGrid.data[index] === originPixelColor[0] &&
+            pixelColorGrid.data[index + 1] === originPixelColor[1] &&
+            pixelColorGrid.data[index + 2] === originPixelColor[2] &&
+            pixelColorGrid.data[index + 3] === originPixelColor[3] 
+        );
+    }
+
+    const fillColor = (index) => {
+        pixelColorGrid.data[index] = color[0];
+        pixelColorGrid.data[index + 1] = color[1];
+        pixelColorGrid.data[index + 2] = color[2];
+        pixelColorGrid.data[index + 3] = color[3];
+    }
+
+    const pixelStack = new Array([x, y]);
+    while(pixelStack.length) {
+        let coord = pixelStack.pop();
+        x = coord[0];
+        y = coord[1];
+
+        let index = (y * canvasWidth + x) * 4;
+        if(x-1 >= 0 && compareColors(index))
+            pixelStack.push([x-1, y]);
+        if(x+1 < canvasWidth && compareColors(index))
+            pixelStack.push([x+1, y]);
+        if(y-1 >= 0 && compareColors(x, y))
+            pixelStack.push([x, y-1]);
+        if(y+1 < canvasHeight && compareColors(index))
+            pixelStack.push([x, y+1]);
+        fillColor(index);
+    }
+
+    ctx.putImageData(pixelColorGrid, 0, 0);
 }
 
 const drawPoint = (ctx, x, y) => {
