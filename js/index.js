2f4b682bbb8380ae0be595c0b84d03bb081b1bee
souravtecken <souravtecken@gmail.com>
2020-03-26 19:49:13 +0530

    Added colorPicker input(for any color) - Added clear canvas tool

--- js/index.js
+++ js/index.js
@@ -29,16 +29,34 @@ function initToolBox(){
         .dropdown();
 }
 
-$(".colorOption").click((event) => {
+$('.colorOption').click((event) => {
+    event.stopPropagation(); // To prevent dropdown from closing
     const child = event.target.querySelector('div');
+    const colorPicker = event.target.querySelector('input');
+    if (colorPicker !== null){
+        $(colorPicker).click();
+        return;
+    }
     let color;
     if(child === null)
-        color = window.getComputedStyle(event.target, null).getPropertyValue("background-color");
+        color = window.getComputedStyle(event.target, null).getPropertyValue('background-color');
     else
-        color = window.getComputedStyle(event.target.querySelector('div'), null).getPropertyValue("background-color");
+        color = window.getComputedStyle(event.target.querySelector('div'), null).getPropertyValue('background-color');
     setStrokeColor(ctx, color);
     setFillColor(ctx, color);
+    $('#colorPaletteTool button').css('color', color);
 });
+
+$('#colorPicker').on('input', ((event) => {
+    const color = event.target.value;
+    setStrokeColor(ctx, color);
+    setFillColor(ctx, color);
+    $('#colorPaletteTool button').css('color', color);
+}))
+
+$('#clearTool').click((event) => {
+    initCanvas(ctx);
+})
 
 initCanvas(ctx);
 initToolBox();
